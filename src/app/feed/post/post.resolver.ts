4c79152431ceed304c546e6ac5303e7076fda1f7
Mauricio Arce <mauricio.arce.mat@gmail.com>
2023-07-05 16:33:03 -0400

    Added logic to resolver, so this redirects to root path if no post details data is found when trying to access this route.

--- src/app/feed/post/post.resolver.ts
+++ src/app/feed/post/post.resolver.ts
@@ -1,12 +1,23 @@
 import { inject } from '@angular/core';
-import { ResolveFn } from '@angular/router';
+import { ResolveFn, Router } from '@angular/router';
+import { EMPTY, catchError } from 'rxjs';
 
-import { PostService } from 'src/app/core/services/feed/post.service';
 import { Post } from './post.model';
+import { PostService } from '../../core/services/feed/post.service';
+import { LoggerService } from '../../core/services/logger/logger.service';
 
 export const postResolver: ResolveFn<Post> = (route, _state) => {
   const postService = inject(PostService);
   const postId = route.paramMap.get('postId') || '';
+  const router = inject(Router);
+  const logger = inject(LoggerService);
 
-  return postService.getPostDetails(postId);
+  return postService.getPostDetails(postId).pipe(
+    catchError((error) => {
+      router.navigate(['/']);
+      logger.error(error);
+
+      return EMPTY;
+    })
+  );
 };
